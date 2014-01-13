d3b4ff38afc0211fd3a24092f7c64517b70e2666
jaimin <jminvaja@gmail.com>
2014-01-13 10:51:42 +0530

    Update print_arr_helper.php
    Removed Hash-Generater and create a new helper for HashMaker

--- print_arr_helper.php
+++ print_arr_helper.php
@@ -74,30 +74,4 @@ if ( ! function_exists('array_push_assoc'))
 	 return $array;
 	}
 }
-if ( ! function_exists('generateRandomString'))
-{
-	function generateRandomString($length = 10){
-	 	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    	$randomString = '';
-	    for ($i = 0; $i < $length; $i++) {
-	        $randomString .= $characters[rand(0, strlen($characters) - 1)];
-	    }
-	    return $randomString;
-	}
-}
-if ( ! function_exists('hashGenerator'))
-{
-	function hashGenerator($str,$salt_len=10){
-		$SALT=generateRandomString($salt_len);
-	 	$hash_password="$SALT".substr(sha1("$SALT"."$str"), 0, -strlen("$SALT"));
-		return $hash_password;
-	}
-}
-if ( ! function_exists('hashGeneratorChecker'))
-{
-	function hashGeneratorChecker($salt,$pwd){
-		$hash_password="$salt".substr(sha1("$salt"."$pwd"), 0, -strlen("$salt"));
-		return $hash_password;
-	}
-}
 ?>
